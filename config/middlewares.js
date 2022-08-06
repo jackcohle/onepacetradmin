77255c3717c3bbbaa12ea65ae749067c007fcac4
Jack Cohle <cohlejack@gmail.com>
2022-08-06 12:37:41 +0300

    Update v1.1 AWS S3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'onepacetr.s3.eu-west-2.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'onepacetr.s3.eu-west-2.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
