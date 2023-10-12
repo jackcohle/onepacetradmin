fc051a9370f27ae8516ed9f58d01bb1a0040d3bf
Sait <cohlejack@gmail.com>
2023-10-12 13:22:36 +0300

    backup

--- src/api/allseason/controllers/allseason.js
+++ src/api/allseason/controllers/allseason.js
@@ -15,7 +15,30 @@ module.exports = createCoreController('api::allseason.allseason', ({strapi}) => 
         const entity = await strapi.db.query('api::allseason.allseason').findOne({
             where: {slug: id},
             populate: {
-                fields: ['*']
+                image: {
+                    populate: {
+                        fields: ['*'],
+                    }
+                },
+                allepisodes: {
+                    populate: {
+                        image: {
+                            populate: {
+                                fields: ['*'],
+                            }
+                        },
+                        allseason: {
+                            populate: {
+                                fields: ['*'],
+                            }
+                        },
+                        alltranslators: {
+                            populate: {
+                                fields: ['*'],
+                            }
+                        }
+                    }
+                }
             }
         });
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
